7dc26cb13fc752acb52543239ab940af1729d5d2
Sanju604 <124423220+Sanju604@users.noreply.github.com>
2024-12-28 14:00:34 +0530

    Update and rename fifth.js to fifth.spec.js

---
+++ Automation/tests/fifth.spec.js
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+var userid;
+
+test('first', async ({ request }) => {
+  const response=await request.post("/booking",
+  {
+data:{ "firstname": "morpheus",
+    "lastname": "leader",
+    "totalprice": 100,
+    "depositpaid": true,
+    "bookingdates": 
+    {
+        "checkin":"2018-01-01",
+        "checkout":"2018-01-01"
+
+    },
+    "additionalneeds":"superbowls"
+    
+
+},
+    headers:{"Accept":"application/json"}
+
+  })
+  const body=await response.headers();
+console.log(body);
+
+});
